55a42d3cc51a050c141b1eee999c79075348f845
far11ven <far11ven@gmail.com>
2023-10-06 17:48:56 +0200

    Final Part 01

--- PART 01/code/scripts/test.js
+++ PART 01/code/scripts/test.js
@@ -2,9 +2,9 @@ import http from 'k6/http';
 
 export let options = {
   stages: [
-    { duration: '10m', target: 50 },
-    { duration: '10m', target: 100 },
-    { duration: '10m', target: 0 } 
+    { duration: '1m', target: 10 },
+    { duration: '1m', target: 20 },
+    { duration: '1m', target: 0 } 
   ],
 };
 
